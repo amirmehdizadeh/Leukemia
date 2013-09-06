0091b63ef2afa1b3498de251930c25f835638398
milad-afsari <afsarimilad@yahoo.com>
2013-09-06 19:05:36 +0430

    Leukemia project first version

---
+++ Leukemia/App_Code/BLL/BLLAnalysis.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DSAnalysisTableAdapters;
+
+/// <summary>
+/// Summary description for BLLAnalysis
+/// </summary>
+/// 
+
+[System.ComponentModel.DataObject]
+public class BLLAnalysis
+{
+    private DSAnalysis.AnalysisDataTable dt;
+    private DSAnalysisTableAdapters.AnalysisTableAdapter ad = null;
+    public DSAnalysisTableAdapters.AnalysisTableAdapter adapter
+    {
+        get
+        {
+            if (ad == null)
+                ad = new AnalysisTableAdapter();
+            return ad;
+        }
+    }
+	public BLLAnalysis()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    //Select Method
+    [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select,true)]
+    public DSAnalysis.AnalysisDataTable Select_Record()
+    {
+        dt = adapter.GetData();
+        return dt;
+    }
+    //Insert Method
+    [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert,true)]
+    public void Insert_Record(int Sh_Bimar, string HB, string WBC, string PLT, string CR, string LFT, string Analysis_Date)
+    {
+        adapter.Insert(Sh_Bimar, HB, WBC, PLT, CR, LFT, common.Shamsi_to_Miladi(Analysis_Date));
+    }
+    //Delete Method
+    [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete,true)]
+    public void Delete_Record(int Original_ShBimar)
+    {
+        adapter.Delete(Original_ShBimar);
+        
+    }
+    //Update Method
+    [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update,true)]
+    public void Update_Record(string hb, string wbc, string plt, string cr, string lft, string analysys_date,int Original_ShBimar)
+    {
+        adapter.Update(hb, wbc, plt, cr, lft, common.Shamsi_to_Miladi(analysys_date), Original_ShBimar);
+    }
+}
---
+++ Leukemia/App_Code/Common/common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+/// <summary>
+/// Summary description for common
+/// </summary>
+public class common
+{
+	public common()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public static string Miladi_to_Shamsi(DateTime d)
+    {
+        PersianCalendar persion = new PersianCalendar();
+        int year, month, day;
+        year = persion.GetYear(d);
+        month = persion.GetMonth(d);
+        day = persion.GetDayOfMonth(d);
+        return (year.ToString() + "/" + month.ToString() + "/" + day.ToString());
+    }
+    public static DateTime Shamsi_to_Miladi(string s)
+    {
+        PersianCalendar persion = new PersianCalendar();
+        string[] d = s.Split('/');
+        return (persion.ToDateTime(int.Parse(d[0]), int.Parse(d[1]), int.Parse(d[2]), 0, 0, 0, 0));
+    }
+}
